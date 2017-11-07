7d941a5d767e08ad62b8b68bf6264f17c63bef56
Jens Alfke <jens@couchbase.com>
2017-11-06 16:35:51 -0800

    Re-indented source code (only whitespace changes)

--- sqlite3_unicodesn_tokenizer.c
+++ sqlite3_unicodesn_tokenizer.c
@@ -17,30 +17,30 @@
 #include "sqlite3_unicodesn_tokenizer.h"
 
 /*
-** Register the tokenizer with FTS3 or FTS4. For use when compiling the tokenizer directly into
-** an application, instead of as a separate shared library.
-*/
+ ** Register the tokenizer with FTS3 or FTS4. For use when compiling the tokenizer directly into
+ ** an application, instead of as a separate shared library.
+ */
 int register_unicodesn_tokenizer(
-      sqlite3 *db          /* The database connection */
+                                 sqlite3 *db          /* The database connection */
 )
 {
-  const sqlite3_tokenizer_module *tokenizer;
-  int rc;
-  sqlite3_stmt *pStmt;
-  const char *zSql = "SELECT fts3_tokenizer(?, ?)";
+    const sqlite3_tokenizer_module *tokenizer;
+    int rc;
+    sqlite3_stmt *pStmt;
+    const char *zSql = "SELECT fts3_tokenizer(?, ?)";
 
-  sqlite3Fts3UnicodeSnTokenizer(&tokenizer);
+    sqlite3Fts3UnicodeSnTokenizer(&tokenizer);
 
-  rc = sqlite3_prepare_v2(db, zSql, -1, &pStmt, 0);
-  if( rc!=SQLITE_OK ){
-    return rc;
-  }
+    rc = sqlite3_prepare_v2(db, zSql, -1, &pStmt, 0);
+    if( rc!=SQLITE_OK ){
+        return rc;
+    }
 
-  sqlite3_bind_text(pStmt, 1, TOKENIZER_NAME, -1, SQLITE_STATIC);
-  sqlite3_bind_blob(pStmt, 2, &tokenizer, sizeof(tokenizer), SQLITE_TRANSIENT);
-  rc = sqlite3_step(pStmt);
-  if( rc!=SQLITE_OK && rc < SQLITE_ROW ){
-      return rc;
-  }
-  return sqlite3_finalize(pStmt);
+    sqlite3_bind_text(pStmt, 1, TOKENIZER_NAME, -1, SQLITE_STATIC);
+    sqlite3_bind_blob(pStmt, 2, &tokenizer, sizeof(tokenizer), SQLITE_TRANSIENT);
+    rc = sqlite3_step(pStmt);
+    if( rc!=SQLITE_OK && rc < SQLITE_ROW ){
+        return rc;
+    }
+    return sqlite3_finalize(pStmt);
 }
